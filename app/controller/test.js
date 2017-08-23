e3a5aed1139ce1ea5f091a659ca9f8d5ce39c875
Flavio Tulino <ftulino@bookingbug.com>
2017-08-23 10:38:50 +0100

    added angular mock; added a deeper test

--- app/controller/test.js
+++ app/controller/test.js
@@ -1,7 +1,18 @@
-class Test {
-    dummy() {
-        return 4;
+class TestComponentController {
+    constructor ($scope, $rootScope, $http) {
+        this.$rootScope = $rootScope;
+        this.$http = $http;
     }
-}
 
-export default Test;
+    getSomething() {
+       return this.$http.get('https://jsonplaceholder.typicode.com/posts/1');
+    }
+
+}
+TestComponentController.$inject = ['$scope', '$rootScope', '$http'];
+
+const testComponent = {
+    controller: TestComponentController
+};
+
+angular.module('testModule', []).component('testComponent', testComponent);
--- app/controller/test.spec.js
+++ app/controller/test.spec.js
@@ -1,13 +1,35 @@
 import Test from "./test";
 
-describe('a test', () => {
-    it('is a test', () => {
-        let t = new Test();
-        expect(t.dummy()).to.equal(4);
+describe('Test Component', () => {
+    let $componentController;
+    let $rootScope;
+
+    beforeEach(angular.mock.module('testModule'));
+    beforeEach(inject(($injector, _$componentController_) => {
+        $componentController = _$componentController_;
+        $rootScope = $injector.get('$rootScope');
+    }));
+
+
+    describe('instance', () => {
+        it('creates an instance', () => {
+            const ctrl = $componentController('testComponent', null, {});
+            return expect(ctrl).to.be.not.undefined;
+        })
     });
 
-    it('a promise test', () => {
-        return expect(Promise.resolve({ foo: "bar" })).to.eventually.have.property("foo");
-    });
+    describe('getSomething', () => {
+        it('returns a value from the server', () => {
+            const ctrl = $componentController('testComponent');
+
+            ctrl.getSomething().then(s => console.log(s));
+
+            ctrl.getSomething = () => {
+                return Promise.resolve({data: {user: 'hello'}});
+            };
+
+            return expect(ctrl.getSomething()).to.eventually.have.property('data').that.has.property('user').that.is.equal('hello');
+        })
+    })
 });
 
